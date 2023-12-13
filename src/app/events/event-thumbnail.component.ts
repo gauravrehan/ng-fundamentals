c3c79c9d54c498efeb74d99e41fca6b53373d615
Gaurav Rehan <Gaurav.Rehan@gmail.com>
2023-12-12 22:03:53 -0500

    module02 - style component elements

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -5,7 +5,7 @@ import { Component, EventEmitter, Input, Output} from "@angular/core";
     templateUrl: './event-thumbnail.component.html',
     styles: [`
     .pad-left {margin-left: 10px;}
-    .well div {color: #bbb;}
+    .well div {color: red;}
     `]
 })
 export class EventThumbnailComponent{
