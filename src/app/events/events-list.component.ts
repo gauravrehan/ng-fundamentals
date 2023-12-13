0eadbb7721954f911305c3edea1b8f894419b12f
Gaurav Rehan <Gaurav.Rehan@gmail.com>
2023-12-13 18:56:51 -0500

    Service basics, Injectable, OnInit event component lifecycle

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,11 +1,11 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { EventService } from "./shared/event.service";
 
 @Component({
 selector: 'events-list',
 templateUrl: './events-list.component.html'
 })
-export class EventsListComponent {
+export class EventsListComponent implements OnInit{
     events:any[];
    constructor(private eventService: EventService){
     
