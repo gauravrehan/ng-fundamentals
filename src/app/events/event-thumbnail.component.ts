2d281e7abd356fd882e45489a9432b3a8dcab4b1
Gaurav Rehan <Gaurav.Rehan@gmail.com>
2023-12-13 16:53:17 -0500

    use class binding to toggle single css class

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,6 +4,8 @@ import { Component, EventEmitter, Input, Output} from "@angular/core";
     selector: 'event-thumbnail',
     templateUrl: './event-thumbnail.component.html',
     styles: [`
+    .green {color: #003300 !important;}
+    
     .thumbnail {min-height: 210px;}
     .pad-left {margin-left: 10px;}
     .well div {color: #bbb;}
