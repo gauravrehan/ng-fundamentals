f19b27b84799f2ed0396d1369f17c574ba0fabd7
Gaurav Rehan <Gaurav.Rehan@gmail.com>
2023-12-12 21:50:40 -0500

    module02 - style component elements

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -2,17 +2,13 @@ import { Component, EventEmitter, Input, Output} from "@angular/core";
 
 @Component({
     selector: 'event-thumbnail',
-    templateUrl: './event-thumbnail.component.html'
+    templateUrl: './event-thumbnail.component.html',
+    styles: [`
+    .pad-left {margin-left: 10px;}
+    .well div {color: #bbb;}
+    `]
 })
 export class EventThumbnailComponent{
-    @Input() event: any;
-
-    someProperty: string = "some value";
-
-    logFoo()
-    {
-        console.log('foo');
-    }
-    
-
+    @Input() event: any
+   
 }
