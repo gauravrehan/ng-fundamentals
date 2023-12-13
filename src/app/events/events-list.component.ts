9f0293ca35530e101c6f6b5eab2443f911611695
Gaurav Rehan <Gaurav.Rehan@gmail.com>
2023-12-12 20:36:21 -0500

    module02 - Pass data to child component

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -5,7 +5,7 @@ selector: 'events-list',
 templateUrl: './events-list.component.html'
 })
 export class EventsListComponent {
-    event = {
+    event1 = {
         id: 1,
         name: 'Angular Connect',
         date: '9/26/2026',
