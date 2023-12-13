20b337f02d05dff02bb5feb8e244268742e7ef5c
Gaurav Rehan <Gaurav.Rehan@gmail.com>
2023-12-13 17:04:23 -0500

    Use ngClass to apply multiple classes

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -5,7 +5,7 @@ import { Component, EventEmitter, Input, Output} from "@angular/core";
     templateUrl: './event-thumbnail.component.html',
     styles: [`
     .green {color: #003300 !important;}
-    
+    .bold {font-weight: bold;}
     .thumbnail {min-height: 210px;}
     .pad-left {margin-left: 10px;}
     .well div {color: #bbb;}
@@ -13,5 +13,26 @@ import { Component, EventEmitter, Input, Output} from "@angular/core";
 })
 export class EventThumbnailComponent{
     @Input() event: any
+
+    getStartTimeClass(){
+        const isEarlyStart = this.event && this.event.time === '8:00 am';
+        return {green: isEarlyStart, bold: isEarlyStart};
+
+        /*ngClass can work with an object like above or it can also work with
+        a) string with space seperated classes
+
+        if(isEarlyStart)
+        return 'green gold'
+        else return '';
+
+        b) with an array of classes
+        
+        if(isEarlyStart)
+        return ['green', 'bold']
+        else retrun []
+        */
+
+
+    }
    
 }
