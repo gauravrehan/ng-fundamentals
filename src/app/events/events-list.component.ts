74f2d3fdfaedd6799f872d8c89e9e5e70f498d63
Gaurav Rehan <Gaurav.Rehan@gmail.com>
2023-12-13 16:25:31 -0500

    Handle nulls in interporation

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -91,11 +91,7 @@ export class EventsListComponent {
           time: '9:00 am',
           price: 950.00,
           imageUrl: '/assets/images/ng-nl.png',
-          location: {
-            address: 'The NG-NL Convention Center & Scuba Shop',
-            city: 'Amsterdam',
-            country: 'Netherlands'
-          },
+          onlineUrl: "https://www.amazon.ca",
           sessions: [
             {
               id: 1,
