81783e9a1c5920610adc8b35e30ca34272a53dcb
Gaurav Rehan <Gaurav.Rehan@gmail.com>
2023-12-14 18:27:24 -0500

    Routes setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+
 import { NavbarComponent } from './nav/navbar.component';
+
 import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
 
+import { appRoutes } from './routes';
+
+
+
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
+    EventDetailsComponent,
     NavbarComponent
   ],
   providers: [
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -4,7 +4,7 @@ import { ToastrService } from "../common/toastr.service";
 
 
 @Component({
-selector: 'events-list',
+// selector: 'events-list',
 templateUrl: './events-list.component.html'
 })
 export class EventsListComponent implements OnInit{
