9d9a0ea8649900c19e2590ddb2f639500b2a2f46
Gaurav Rehan <Gaurav.Rehan@gmail.com>
2023-12-13 19:33:19 -0500

    wrapping third party services

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { EventService } from "./shared/event.service";
+import { ToastrService } from "../common/toastr.service";
+
 
 @Component({
 selector: 'events-list',
@@ -7,14 +9,16 @@ templateUrl: './events-list.component.html'
 })
 export class EventsListComponent implements OnInit{
     events:any[];
-   constructor(private eventService: EventService){
-    
+
+   constructor(private eventService: EventService, private toastrService: ToastrService){
    }
 
    ngOnInit(){
     this.events = this.eventService.getEvents();
    }
 
-   
-    
+   handleThumbnailClick(eventName){
+    console.log("clicked");
+    this.toastrService.success(eventName);
+   }
 }
