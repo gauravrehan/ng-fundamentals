7f8d223082a81d9f1263ee8406dbfb05cc46f957
Gaurav Rehan <Gaurav.Rehan@gmail.com>
2023-12-14 20:06:08 -0500

    Use barrels to organize imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventService,
+  EventRouteActivator,
+  EventListResolver
+} from './events/index'
+
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
-import { CreateEventComponent } from './events/create-event.component';
-
 import { NavbarComponent } from './nav/navbar.component';
-
-import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-
 import { appRoutes } from './routes';
-
 import { Error404Component } from './error/404.component';
-import { EventListResolver } from './events/events-list-resolver.service';
-
-
-
-
 
 @NgModule({
   imports: [
