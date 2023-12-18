03f63d924e6aacc64df3a1654a66c3fbbb46a44c
Gaurav Rehan <Gaurav.Rehan@gmail.com>
2023-12-17 20:44:04 -0500

    Better dependency injection of global service like Toastr using Injection_Token

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from "@angular/core";
 import { EventService } from "./shared/event.service";
-import { ToastrService } from "../common/toastr.service";
 import { ActivatedRoute } from "@angular/router";
 import { IEvent } from "./shared/index";
 
@@ -11,15 +10,12 @@ templateUrl: './events-list.component.html'
 export class EventsListComponent implements OnInit{
     events: IEvent[];
 
-   constructor(private eventService: EventService, private toastrService: ToastrService, private route: ActivatedRoute){
+   constructor(private eventService: EventService, private route: ActivatedRoute){
    }
 
    ngOnInit(){
     this.events = this.route.snapshot.data['events'];
    }
 
-   handleThumbnailClick(eventName){
-    console.log("clicked");
-    this.toastrService.success(eventName);
-   }
+   
 }
