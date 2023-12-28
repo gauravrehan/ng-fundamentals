55ca75b44a5716c71690c06607f406bfbb64c032
Gaurav Rehan <Gaurav.Rehan@gmail.com>
2023-12-27 23:44:04 -0500

    setting up local http server and get data from events http service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,7 @@ import { appRoutes } from './routes';
 import { Error404Component } from './error/404.component';
 import { AuthService } from './user/auth.service';
 
+import { HttpClientModule } from '@angular/common/http'
 
 let toastr:Toastr = window['toastr'];
 let jQuery = window['$'];
@@ -36,7 +37,8 @@ let jQuery = window['$'];
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   declarations: [
     EventsAppComponent,
